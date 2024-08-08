8deb9e1c162ad474392830a83c48c200ce3d31c1
JC Ang <angang9424@gmail.com>
2024-08-08 23:49:37 +0800

    fix error

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const purchaseOrderRouter = require('./routes/purchaseorderroutes');
 
 app.use("/api/v1/books", bookRouter);
 app.use("/api/v1/users", userRouter);
-app.use("/api/v1/purchaseOrders", userRouter);
+app.use("/api/v1/purchaseOrders", purchaseOrderRouter);
 
 app.listen(process.env.PORT, () => {
 	console.log("Server running 3001.");
