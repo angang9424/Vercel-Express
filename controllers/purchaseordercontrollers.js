dc13308eaca9501875d62f81fdd458f6f514f02d
JC Ang <angang9424@gmail.com>
2024-08-10 18:52:40 +0800

    add create query for child table

--- controllers/purchaseordercontrollers.js
+++ controllers/purchaseordercontrollers.js
@@ -1,76 +1,89 @@
 const postgre = require('../database');
 
 const bookController = {
-    getAll: async(req, res) => {
-        try {
-            const { rows } = await postgre.query("select * from purchase_order");
-            res.json({msg: "OK", data: rows});
-        } catch (error) {
-            res.json({msg: error.msg});
-        }
-    },
-    getName: async(req, res) => {
-        try {
-            const { rows } = await postgre.query("select name from purchase_order");
-            res.json({msg: "OK", data: rows});
-        } catch (error) {
-            res.json({msg: error.msg});
-        }
-    },
-    getById: async(req, res) => {
-        try {
-            const { rows } = await postgre.query("select * from purchase_order where book_id = $1", [req.params.id]);
+	getAll: async(req, res) => {
+		try {
+			const { rows } = await postgre.query("select * from purchase_order");
+			res.json({msg: "OK", data: rows});
+		} catch (error) {
+			res.json({msg: error.msg});
+		}
+	},
+	getName: async(req, res) => {
+		try {
+			const { rows } = await postgre.query("select name from purchase_order");
+			res.json({msg: "OK", data: rows});
+		} catch (error) {
+			res.json({msg: error.msg});
+		}
+	},
+	getById: async(req, res) => {
+		try {
+			const { rows } = await postgre.query("select * from purchase_order where book_id = $1", [req.params.id]);
 
-            if (rows[0]) {
-                return res.json({msg: "OK", data: rows});
-            }
+			if (rows[0]) {
+				return res.json({msg: "OK", data: rows});
+			}
 
-            res.status(404).json({msg: "not found"});
-        } catch (error) {
-            res.json({msg: error.msg});
-        }
-    },
-    create: async(req, res) => {
-        try {
-            const { date, total_amount, created_modified_by, modified } = req.body;
+			res.status(404).json({msg: "not found"});
+		} catch (error) {
+			res.json({msg: error.msg});
+		}
+	},
+	create: async(req, res) => {
+		try {
+			const { date, total_amount, created_modified_by, modified } = req.body;
 
-            const sql = 'INSERT INTO purchase_order(date, total_amount, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5) RETURNING *';
+			const sql = 'INSERT INTO purchase_order(date, total_amount, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5) RETURNING *';
 
-            const { rows } = await postgre.query(sql, [date, total_amount, created_modified_by, created_modified_by, modified]);
+			const { rows } = await postgre.query(sql, [date, total_amount, created_modified_by, created_modified_by, modified]);
 
-            res.json({msg: "OK", data: rows[0]});
-        } catch (error) {
-            res.json({msg: error.msg});
-        }
-    },
-    updateById: async(req, res) => {
-        try {
-            const { name, price, modified } = req.body;
+			res.json({msg: "OK", data: rows[0]});
+		} catch (error) {
+			res.json({msg: error.msg});
+		}
+	},
+	createchild: async(req, res) => {
+		try {
+			const { item, qty, rate, amount, order_id, created_modified_by, modified } = req.body;
 
-            const sql = 'UPDATE purchase_order set name = $1, price = $2, modified = $3 where book_id = $4 RETURNING *';
+			const sql = 'INSERT INTO purchase_order_item(item, qty, rate, amount, order_id, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
 
-            const { rows } = await postgre.query(sql, [name, price, modified, req.params.id]);
+			const { rows } = await postgre.query(sql, [item, qty, rate, amount, order_id, created_modified_by, created_modified_by, modified]);
 
-            res.json({msg: "OK", data: rows[0]});
-        } catch (error) {
-            res.json({msg: error.msg})
-        }
-    },
-    deleteById: async(req, res) => {
-        try {
-            const sql = 'DELETE FROM purchase_order where book_id = $1 RETURNING *';
+			res.json({msg: "OK", data: rows[0]});
+		} catch (error) {
+			res.json({msg: error.msg});
+		}
+	},
+	updateById: async(req, res) => {
+		try {
+			const { name, price, modified } = req.body;
 
-            const { rows } = await postgre.query(sql, [req.params.id]);
+			const sql = 'UPDATE purchase_order set name = $1, price = $2, modified = $3 where book_id = $4 RETURNING *';
 
-            if (rows[0]) {
-                return res.json({msg: "OK", data: rows[0]});
-            }
+			const { rows } = await postgre.query(sql, [name, price, modified, req.params.id]);
 
-            return res.status(404).json({msg: "not found"});
-        } catch (error) {
-            res.json({msg: error.msg})
-        }
-    }
+			res.json({msg: "OK", data: rows[0]});
+		} catch (error) {
+			res.json({msg: error.msg})
+		}
+	},
+	deleteById: async(req, res) => {
+		try {
+			const sql = 'DELETE FROM purchase_order where book_id = $1 RETURNING *';
+
+			const { rows } = await postgre.query(sql, [req.params.id]);
+
+			if (rows[0]) {
+				return res.json({msg: "OK", data: rows[0]});
+			}
+
+			return res.status(404).json({msg: "not found"});
+		} catch (error) {
+			res.json({msg: error.msg})
+		}
+	}
 }
 
 module.exports = bookController
--- routes/purchaseorderroutes.js
+++ routes/purchaseorderroutes.js
@@ -7,6 +7,7 @@ router.get("/", bookController.getAll);
 router.get("/bookname", bookController.getName);
 router.get("/:id", bookController.getById);
 router.post("/", bookController.create);
+router.post("/createchild", bookController.createchild);
 router.put("/:id", bookController.updateById);
 router.delete("/:id", bookController.deleteById);
 
