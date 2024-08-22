f20dda2266181a4fe06b1cc833b6372c542a421b
JC Ang <angang9424@gmail.com>
2024-08-22 18:22:25 +0800

    test error

--- controllers/purchaseordercontrollers.js
+++ controllers/purchaseordercontrollers.js
@@ -112,11 +112,15 @@ const poController = {
 
 			const { rows } = await postgre.query(sql, [req.params.id]);
 
-			// for (const row of rows) {
-			// 	const bin_sql = 'UPDATE bin set qty = qty - $1, modified_by = $2, modified = $3 where item_id = $4';
+			for (const row of rows) {
+				const bin_sql = 'UPDATE bin set qty = qty - $1, modified_by = $2, modified = $3 where item_id = $4';
 
-			// 	await postgre.query(bin_sql, [row.qty, modified_by, modified, row.item]);
-			// };
+				try {
+					await postgre.query(bin_sql, [row.qty, modified_by, modified, row.item]);
+				} catch (error) {
+					return res.json({msg: error, data: 'modified_by'})
+				}
+			};
 
 			// if (rows[0]) {
 			// 	return res.json({msg: "OK", data: rows[0]});
