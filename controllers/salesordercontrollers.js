0592f1432c600edc4da7377c0a7d173ece0c79fa
JC Ang <angang9424@gmail.com>
2024-08-27 22:21:35 +0800

    update the create so query

--- controllers/salesordercontrollers.js
+++ controllers/salesordercontrollers.js
@@ -31,12 +31,36 @@ const soController = {
 		}
 	},
 	create: async(req, res) => {
+		const client = await postgre.connect();
+
 		try {
-			const { date, total_amount, created_modified_by, modified } = req.body;
+			await client.query('BEGIN');
+
+			const { date, total_amount, items, created_modified_by, modified } = req.body;
 
 			const sql = 'INSERT INTO sales_order(date, total_amount, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5) RETURNING *';
 
-			const { rows } = await postgre.query(sql, [date, total_amount, created_modified_by, created_modified_by, modified]);
+			const { rows } = await client.query(sql, [date, total_amount, created_modified_by, created_modified_by, modified]);
+
+			for (const item of items) {
+
+				const { idx, item, qty, stock_qty, rate, amount, order_id, created_modified_by, modified } = req.body;
+
+				const child_sql = 'INSERT INTO sales_order_item(idx, item, qty, stock_qty, rate, amount, order_id, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
+
+				await client.query(child_sql, [item.idx, item.item, item.qty, item.stock_qty, item.rate, item.amount, rows[0].id, created_modified_by, created_modified_by, modified]);
+
+				const bin_sql = 'UPDATE bin set qty = qty - $1, modified_by = $2, modified = $3 where item_id = $4 RETURNING *';
+
+				const { rows: binRrows } = await client.query(bin_sql, [qty, created_modified_by, modified, item]);
+
+				if (binRrows[0].qty >= 0) {
+					await client.query('COMMIT');
+				} else {
+					await client.query('ROLLBACK');
+					return res.status(400).json({msg: "not enough stock"});
+				}
+			}
 
 			res.json({msg: "OK", data: rows[0]});
 		} catch (error) {
@@ -44,16 +68,27 @@ const soController = {
 		}
 	},
 	createChild: async(req, res) => {
+		const client = await postgre.connect();
+
 		try {
+			await client.query('BEGIN');
+
 			const { idx, item, qty, stock_qty, rate, amount, order_id, created_modified_by, modified } = req.body;
 
 			const sql = 'INSERT INTO sales_order_item(idx, item, qty, stock_qty, rate, amount, order_id, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
 
-			const { rows } = await postgre.query(sql, [idx, item, qty, stock_qty, rate, amount, order_id, created_modified_by, created_modified_by, modified]);
+			const { rows } = await client.query(sql, [idx, item, qty, stock_qty, rate, amount, order_id, created_modified_by, created_modified_by, modified]);
 
 			const bin_sql = 'UPDATE bin set qty = qty - $1, modified_by = $2, modified = $3 where item_id = $4 RETURNING *';
 
-			await postgre.query(bin_sql, [qty, created_modified_by, modified, item]);
+			const { rows: binRrows } = await client.query(bin_sql, [qty, created_modified_by, modified, item]);
+
+			if (binRrows[0].qty >= 0) {
+				await client.query('COMMIT');
+			} else {
+				await client.query('ROLLBACK');
+				return res.status(400).json({msg: "not enough stock"});
+			}
 
 			// if (rows[0]) {
 			// 	req.body = {item_id: item, qty: qty, modified_by: created_modified_by, modified: modified};
@@ -61,6 +96,7 @@ const soController = {
 			// }
 			res.json({msg: "OK", data: rows[0]});
 		} catch (error) {
+			await client.query('ROLLBACK');
 			res.json({msg: error.msg});
 		}
 	},
