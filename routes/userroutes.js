02fa5f33a705c6358220383eaab58c356c64621d
JC Ang <angang9424@gmail.com>
2024-09-03 15:25:08 +0800

    remove the test upload img

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const multer = require('multer');
 
 const userController = require('../controllers/usercontrollers');
-const upload = multer({ dest: '../uploads/' });
 
 router.get("/", userController.getAll);
 router.post("/userLogin", userController.userLogin);
 router.post("/getUserDetails/:id", userController.getUserDetails);
 router.post("/", userController.create);
-router.post("/userpropic", upload.single('file'), userController.userProPic);
 router.put("/updatePassword/:id", userController.updatePassword);
 router.put("/updateUserDetails/:id", userController.updateUserDetails);
 // router.delete("/:id", userController.deleteById);
