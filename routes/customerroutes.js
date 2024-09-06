e4bc2c5da97e8e0a3728563c39cf019c3e7263ed
JC Ang <angang9424@gmail.com>
2024-09-06 14:55:42 +0800

    add customer query

--- routes/customerroutes.js
+++ routes/customerroutes.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const customerController = require('../controllers/customercontrollers');
 
 router.get("/", customerController.getAll);
-router.get("/:id", customerController.getById);
+// router.get("/:id", customerController.getById);
 router.post("/", customerController.create);
-router.put("/:id", customerController.updateById);
-router.delete("/:id", customerController.deleteById);
+// router.put("/:id", customerController.updateById);
+// router.delete("/:id", customerController.deleteById);
 
 module.exports = router;
