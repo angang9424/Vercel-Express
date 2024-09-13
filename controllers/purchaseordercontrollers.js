123dd557de481ccc6805883676cde742f9ba2362
JC Ang <angang9424@gmail.com>
2024-09-13 15:45:11 +0800

    update the create PO child, to update or add item price

--- controllers/purchaseordercontrollers.js
+++ controllers/purchaseordercontrollers.js
@@ -45,17 +45,23 @@ const poController = {
 	},
 	createChild: async(req, res) => {
 		try {
-			const { rows, order_id, created_modified_by, modified } = req.body;
+			const { rows, order_id, supplier_id, supplier_name, date, created_modified_by, modified } = req.body;
 
 			const sql = 'INSERT INTO purchase_order_item(idx, item_id, item_name, qty, rate, amount, order_id, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
 			const bin_sql = 'UPDATE bin set qty = qty + $1, modified_by = $2, modified = $3 where item_id = $4 RETURNING *';
 
 			for (const row of rows) {
-				const { rows: po_item } = await postgre.query(sql, [row.idx, row.item_id, row.item_name, row.qty, row.rate, row.amount, order_id, created_modified_by, created_modified_by, modified]);
-				row.id = po_item[0].id;
-				const { rows: bin_item } = await postgre.query(bin_sql, [row.qty, created_modified_by, modified, row.item_id]);
+				const { rows: PORows } = await postgre.query(sql, [row.idx, row.item_id, row.item_name, row.qty, row.rate, row.amount, order_id, created_modified_by, created_modified_by, modified]);
+				row.id = PORows[0].id;
 
-				if (!bin_item || bin_item.length === 0) {
+				if (row.price_list_rate != row.rate) {
+					const item_price_sql = 'INSERT INTO item_price(item_id, item_name, price_type, party_id, party_name, rate, valid_from, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+					const { rows: itemPriceRrows } = await postgre.query(item_price_sql, [row.item_id, row.item_name, 'Buying', supplier_id, supplier_name, row.rate, date, created_modified_by, modified, row.item_id]);
+				}
+
+				const { rows: binRrows } = await postgre.query(bin_sql, [row.qty, created_modified_by, modified, row.item_id]);
+
+				if (!binRrows || binRrows.length === 0) {
 					const create_bin_sql = 'INSERT INTO bin(item_id, item_name, qty, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
 
 					await postgre.query(create_bin_sql, [row.item_id, row.item_name, row.qty, created_modified_by, created_modified_by, modified]);
