c85258f167ea28c05c0ec658b53b934d290392a9
JC Ang <angang9424@gmail.com>
2024-09-19 16:24:00 +0800

    add missing value

--- controllers/salesordercontrollers.js
+++ controllers/salesordercontrollers.js
@@ -36,34 +36,34 @@ const soController = {
 		try {
 			await client.query('BEGIN');
 
-			const { customer_id, customer_name, date, total_amount, items, created_modified_by, modified } = req.body;
+			const { customer_id, customer_name, date, total_amount, rows, created_modified_by, modified } = req.body;
 
 			const sql = 'INSERT INTO sales_order(customer_id, customer_name, date, total_amount, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7) RETURNING *';
 
-			const { rows } = await client.query(sql, [customer_id, customer_name, date, total_amount, created_modified_by, created_modified_by, modified]);
+			const { rows: SORrows } = await client.query(sql, [customer_id, customer_name, date, total_amount, created_modified_by, created_modified_by, modified]);
 
-			for (const item of items) {
-				const child_sql = 'INSERT INTO sales_order_item(idx, item_id, item_name, qty, stock_qty, rate, price_list_rate, amount, order_id, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING *';
+			for (const row of rows) {
+				const child_sql = 'INSERT INTO sales_order_item(idx, item_id, item_name, qty, stock_qty, rate, price_list_rate, amount, order_id, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12) RETURNING *';
 
-				await client.query(child_sql, [item.idx, item.item_id, item.item_name, item.qty, item.stock_qty, item.rate, item.price_list_rate, item.amount, rows[0].id, created_modified_by, created_modified_by, modified]);
+				await client.query(child_sql, [row.idx, row.item_id, row.item_name, row.qty, row.stock_qty, row.rate, row.price_list_rate, row.amount, rows[0].id, created_modified_by, created_modified_by, modified]);
 
 				const bin_sql = 'UPDATE bin set qty = qty - $1, modified_by = $2, modified = $3 where item_id = $4 RETURNING *';
 
-				const { rows: binRrows } = await client.query(bin_sql, [item.qty, created_modified_by, modified, item.item_id]);
+				const { rows: binRrows } = await client.query(bin_sql, [row.qty, created_modified_by, modified, row.item_id]);
 
 				if (binRrows[0].qty < 0) {
 					await client.query('ROLLBACK');
 					return res.status(400).json({msg: "not enough stock", data: {items: items}});
 				}
 
-				if (parseFloat(item.price_list_rate) != parseFloat(item.rate)) {
+				if (parseFloat(row.price_list_rate) != parseFloat(row.rate)) {
 					const item_price_sql = 'INSERT INTO item_price(item_id, item_name, price_type, party_id, party_name, rate, valid_from, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *';
-					const { rows: itemPriceRrows } = await postgre.query(item_price_sql, [item.item_id, item.item_name, 'Selling', customer_id, customer_name, item.rate, date, created_modified_by, created_modified_by, modified]);
+					const { rows: itemPriceRrows } = await postgre.query(item_price_sql, [row.item_id, row.item_name, 'Selling', customer_id, customer_name, row.rate, date, created_modified_by, created_modified_by, modified]);
 				}
 			}
 
 			await client.query('COMMIT');
-			res.json({msg: "OK", data: rows[0]});
+			res.json({msg: "OK", data: SORrows[0]});
 		} catch (error) {
 			await client.query('ROLLBACK');
 			res.json({msg: error.msg});
