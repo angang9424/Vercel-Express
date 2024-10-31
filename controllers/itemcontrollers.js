cab338d8ac60241b6a8a20a283328c49e850d7c6
JC Ang <angang9424@gmail.com>
2024-10-31 12:23:35 +0800

    add column for expense and income account

--- controllers/itemcontrollers.js
+++ controllers/itemcontrollers.js
@@ -73,11 +73,11 @@ const itemController = {
 	},
 	create: async(req, res) => {
 		try {
-			const { name, item_category, default_account, created_modified_by, modified } = req.body;
+			const { name, item_category, default_expense_account, default_income_account, created_modified_by, modified } = req.body;
 
-			const sql = 'INSERT INTO item(name, category, default_account, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
+			const sql = 'INSERT INTO item(name, category, default_expense_account, default_income_account, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5, $6) RETURNING *';
 
-			const { rows } = await postgre.query(sql, [name, item_category, default_account, created_modified_by, created_modified_by, modified]);
+			const { rows } = await postgre.query(sql, [name, item_category, default_expense_account, default_income_account, created_modified_by, created_modified_by, modified]);
 
 			const bin_sql = 'INSERT INTO bin(item_id, item_name, created_by, modified_by, modified) VALUES($1, $2, $3, $4, $5) RETURNING *';
 
